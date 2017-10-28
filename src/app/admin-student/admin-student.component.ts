e9c143dd21f880975ff9016819cce6606b7c641f
Kashif Roshen <kashifroshen@MacBooks-MacBook-Pro.local>
2017-10-28 12:33:58 +0530

    admin student view added

--- src/app/admin-student/admin-student.component.ts
+++ src/app/admin-student/admin-student.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StudentService } from '../services/student/student.service';
+
 @Component({
   selector: 'app-admin-student',
   templateUrl: './admin-student.component.html',
   styleUrls: ['./admin-student.component.css']
 })
 export class AdminStudentComponent implements OnInit {
+  students: any;
 
-  constructor() { }
+  constructor(private _studentService: StudentService) { }
 
   ngOnInit() {
   }
 
+  private getStudents() {
+    this._studentService.getStudents().subscribe(students => {
+      this.students = students;
+      console.log("students are", this.students);
+    });
+  }
+ 
 }
